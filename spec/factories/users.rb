fa0872af022452efd1a5bfd8a54a897519a7ff98
taku113 <onepiece646@gmail.com>
2022-04-21 21:26:07 +0900

    単体テストコードの記述

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {'test'}
-    email                 {'test@example'}
-    password              {'000000'}
+    nickname {Faker::Name.name}
+    first_name {Faker::Name.first_name}
+    first_name_kana {"カタカナ"}
+    family_name {Faker::Name.last_name}
+    family_name_kana {"カタカナ"}
+    email {Faker::Internet.free_email}
+    password {Faker::Internet.password(min_length: 6)}
     password_confirmation {password}
+    birthday {"1945-01-01"}
   end
 end

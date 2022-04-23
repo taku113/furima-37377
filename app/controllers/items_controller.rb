793a176d0b453530321ea1636e19a7883711a409
taku113 <onepiece646@gmail.com>
2022-04-23 16:26:19 +0900

    new,createアクションのルーティングの設定とストロングパラメーターの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,4 +2,18 @@ class ItemsController < ApplicationController
   # before_action :authenticate_user!
   def index
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+  end
+
+  private 
+  def item_params
+    params.require(:item).permit(:item_name, :explanation, :category_id, :condition_id, :postage_id, :prefecture_id, :delivery_time_id, :user_id, :price)
+  end
+
 end

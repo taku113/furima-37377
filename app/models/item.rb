a315de4338f43502f0c3254b2c2c6d542d2617c1
taku113 <onepiece646@gmail.com>
2022-04-23 19:19:11 +0900

    itemモデルのバリデーションの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,11 @@
 class Item < ApplicationRecord
+  validates :item_name, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :postage_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :delivery_time_id, presence: true
+  validates :user_id, presence: true
+  validates :price, presence: true
 end

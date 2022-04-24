49cb3004e1f0f2650b5bbb55e383ef4fc9453b0e
taku113 <onepiece646@gmail.com>
2022-04-24 11:42:29 +0900

    価格のバリデーションの記載

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,5 +7,6 @@ class Item < ApplicationRecord
   validates :prefecture_id, presence: true
   validates :delivery_time_id, presence: true
   validates :user_id, presence: true
-  validates :price, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 9_999_999 },
+  format: { with: /\A[0-9]+\z/ }
 end

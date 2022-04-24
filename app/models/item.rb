13d75684b3e70762c3a86a03e57a4e9292e09a2d
taku113 <onepiece646@gmail.com>
2022-04-24 12:09:37 +0900

    バリデーションの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,14 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category, :condition, :postage, :prefecture, :deryvery_time 
+
   validates :item_name, presence: true, length: { maximum: 40}
   validates :explanation, presence: true, length: { maximum: 1000}
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :postage_id, presence: true
-  validates :prefecture_id, presence: true
-  validates :delivery_time_id, presence: true
+  validates :category_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :condition_id, presence: true, numericality: { other_than: 1 , message: "can't be blank" }
+  validates :postage_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
+  validates :delivery_time_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :user_id, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 9_999_999 },
   format: { with: /\A[0-9]+\z/ }

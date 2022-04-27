61bd7c602f969ecb705f26e1fab6b6be909ad568
taku113 <onepiece646@gmail.com>
2022-04-28 07:53:05 +0900

    単体テストコード追記

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -86,6 +86,11 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include("Price must be an integer")
       end
 
+      it "userが紐付いていなければ出品できない" do
+        @item.user = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("User must exist")
+      end
     end
   end
 end

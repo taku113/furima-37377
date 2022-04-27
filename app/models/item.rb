a2a5c9c6df48a16872baafb74bad2e6368d2fb25
taku113 <onepiece646@gmail.com>
2022-04-27 20:38:59 +0900

    単体テストの記述

--- app/models/item.rb
+++ app/models/item.rb
@@ -17,6 +17,5 @@ class Item < ApplicationRecord
   validates :prefecture_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :delivery_time_id, presence: true, numericality: { other_than: 1 , message: "can't be blank"}
   validates :image, presence: true
-  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 9_999_999 },
-  format: { with: /\A[0-9]+\z/ }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer: true }, format: { with: /\A[0-9]+\z/ }
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -18,6 +18,73 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Image can't be blank")
       end
+
+      it "商品名が空では登録できない" do
+        @item.item_name = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Item name can't be blank")
+      end
+
+      it "商品説明が空では登録できない" do
+        @item.explanation = ""
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Explanation can't be blank")
+      end
+
+      it "カテゴリーが未選択だと登録できない" do
+        @item.category_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category can't be blank")
+      end
+
+      it "商品の状態が未選択だと登録できない" do
+        @item.condition_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition can't be blank")
+      end
+
+      it "配送料の負担が未選択だと登録できない" do
+        @item.postage_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Postage can't be blank")
+      end
+
+      it "発送元の地域が未選択だと登録できない" do
+        @item.prefecture_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+      end
+
+      it "発送までの日数が未選択だと登録できない" do
+        @item.delivery_time_id = "1"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Delivery time can't be blank")
+      end
+
+      it "金額が未入力の場合は登録できない" do
+        @item.price = nil
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price can't be blank")
+      end
+
+      it "金額が300円以下の場合は登録できない" do
+        @item.price = "100"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+      end
+
+      it "金額が1000万円以上の場合は登録できない" do
+        @item.price = "10000000"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+      end
+
+      it "金額に小数点が含まれる場合は登録できない" do
+        @item.price = "1000.456"
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be an integer")
+      end
+
     end
   end
 end

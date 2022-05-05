e3d0c372d25175b73ddb58b4b72f5f52eeeba33f
taku113 <onepiece646@gmail.com>
2022-05-05 14:29:35 +0900

    itemコントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:create, :new]
   def index
-    @item = Item.all
+    @items = Item.all
   end
 
   def new

e8423aaf5a49369072fadc16a1f572f55fe3a88c
taku113 <onepiece646@gmail.com>
2022-05-07 17:18:25 +0900

    出品者のみ編集できるように制限

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :new]
+  before_action :authenticate_user!, only: [:create, :new, :edit]
+  before_action :move_to_index, only: [:edit]
+
   def index
     @items = Item.order("created_at DESC")
   end
@@ -40,4 +42,10 @@ class ItemsController < ApplicationController
     params.require(:item).permit(:item_name, :explanation, :category_id, :condition_id, :postage_id, :prefecture_id, :delivery_time_id, :user_id, :price, :image).merge(user_id: current_user.id)
   end
 
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless @item.user.id == current_user.id
+      redirect_to action: :index
+    end
+  end
 end

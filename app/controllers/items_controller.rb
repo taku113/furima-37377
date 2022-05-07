d86f8d19b1116d2c862e551cfc088c9a086fcbac
taku113 <onepiece646@gmail.com>
2022-05-07 15:25:54 +0900

    editアクションの定義

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -21,6 +21,11 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+  
+
   private 
   def item_params
     params.require(:item).permit(:item_name, :explanation, :category_id, :condition_id, :postage_id, :prefecture_id, :delivery_time_id, :user_id, :price, :image).merge(user_id: current_user.id)

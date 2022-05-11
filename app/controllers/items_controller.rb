bfc02fd0de3749b8c900e8d4c25027e205abe960
taku113 <onepiece646@gmail.com>
2022-05-11 19:49:31 +0900

    destroyアクションの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:create, :new, :edit]
+  before_action :authenticate_user!, only: [:create, :new, :edit, :destroy]
   before_action :item_find, only: [:show, :edit, :update, :destroy, :move_to_index]
-  before_action :move_to_index, only: [:edit, :destroy]
+  before_action :move_to_index, only: [:edit ]
   
 
   def index

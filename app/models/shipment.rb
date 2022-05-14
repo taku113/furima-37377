d1fe2f145d9bdca78e7f5834a43074c19696cd2f
taku113 <onepiece646@gmail.com>
2022-05-14 16:10:16 +0900

    shipmentモデルのバリデーションのコメントアウト

--- app/models/shipment.rb
+++ app/models/shipment.rb
@@ -1,9 +1,9 @@
 class Shipment < ApplicationRecord
   belongs_to :purchase
 
-  validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
-  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
-  validates :city, presence: true
-  validates :address, presence: true
-  validates :phone_number, presence: true, numericality: true
+  #validates :postal_code, presence: true, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+  #validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
+  #validates :city, presence: true
+  #validates :address, presence: true
+  #validates :phone_number, presence: true, numericality: true
 end

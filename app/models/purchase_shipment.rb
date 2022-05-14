4a2ced75e7561ed0defaeed3b23b06786f1e3734
taku113 <onepiece646@gmail.com>
2022-05-14 15:58:06 +0900

    Formオブジェクトへバリデーションの記述

--- app/models/purchase_shipment.rb
+++ app/models/purchase_shipment.rb
@@ -1,4 +1,12 @@
 class PurchaseShipment
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number
+
+  with_options presence: true do
+    validates :postal_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city
+    validates :address
+    validates :phone_number, numericality: true
+  end
+  validates :prefecture_id, numericality: {other_than: 0, message: "can't be blank"}
 end

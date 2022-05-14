df0b56572617bdc04372ff2d067dea3bdca44097
taku113 <onepiece646@gmail.com>
2022-05-14 11:56:23 +0900

    attr_accessorの記述

--- app/models/purchase_shipment.rb
+++ app/models/purchase_shipment.rb
@@ -1,2 +1,4 @@
-class DonationAddress
+class PurchaseShipment
+  include ActiveModel::Model
+  attr_accessor :user_id, :item_id, :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number
 end

8478acbc99066f2b673791f9a4c0e0d984575a72
taku113 <onepiece646@gmail.com>
2022-05-14 17:58:18 +0900

    コントローラーの設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -2,6 +2,22 @@ class PurchasesController < ApplicationController
 
   def index
     #フォームオブジェクトのインスタンスを生成し、インスタンス変数に代入する
-
+    @purchase_shipment = PurchaseShipment
   end
+
+  def create
+    @purchase_shipment = PurchaseShipment.new(purchase_params)
+    if @purchase_shipment.valid?
+      @purchase_shipment.save
+      redirect_to root_path
+    else
+      reder :new
+    end
+
+    private
+
+    def purchase_params
+      params.require(:purchase_shipment).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id)
+    end
+
 end

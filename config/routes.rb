3a86a16034ba0af5100e57eec334e40c37084396
taku113 <onepiece646@gmail.com>
2022-05-14 18:23:25 +0900

    購入画面のビューの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'items#index'
   resources :items, only: [:new, :create, :index, :show, :edit, :update, :destroy] do
-    resources: purchases, only: [:index, :create]
+    resources :purchases, only: [:index, :create]
   end
 end

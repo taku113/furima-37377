0bdcf8bc4e6e9510fd10a662c9e3c663231090b1
taku113 <onepiece646@gmail.com>
2022-05-27 20:48:40 +0900

    決済処理のリファクタリング

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -10,12 +10,6 @@ class PurchasesController < ApplicationController
     @item = Item.find(params[:item_id])
     @purchase_shipment = PurchaseShipment.new(purchase_params)
     if @purchase_shipment.valid?
-      Payjp.api_key = "sk_test_5b23ecb45b4205b86781e3e2"  # PAY.JPテスト秘密鍵
-      Payjp::Charge.create(
-        amount: @item.price,  # 商品の値段
-        card: purchase_params[:token],    # カードトークン
-        currency: 'jpy'                 # 通貨の種類（日本円）
-      )
       @purchase_shipment.save
       redirect_to root_path
     else
@@ -28,5 +22,15 @@ class PurchasesController < ApplicationController
     def purchase_params
       params.require(:purchase_shipment).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: @item.id, token: params[:token])
     end
+
+    def pay_item
+      Payjp.api_key = "sk_test_5b23ecb45b4205b86781e3e2"  # PAY.JPテスト秘密鍵
+      Payjp::Charge.create(
+        amount: @item.price,  # 商品の値段
+        card: purchase_params[:token],    # カードトークン
+        currency: 'jpy'                 # 通貨の種類（日本円）
+      )
+    end
+
 
 end

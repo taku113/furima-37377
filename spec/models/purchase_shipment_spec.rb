a72eeb1f90291aec8243d7d28cd2f202f3981f9a
taku113 <onepiece646@gmail.com>
2022-06-01 20:42:53 +0900

    item情報のテストコード追加

--- spec/models/purchase_shipment_spec.rb
+++ spec/models/purchase_shipment_spec.rb
@@ -86,6 +86,12 @@ RSpec.describe PurchaseShipment, type: :model do
         @purchase_shipment.valid?
         expect(@purchase_shipment.errors.full_messages).to include("User can't be blank")
       end
+
+      it "itemが紐づいていないと購入できない" do
+        @purchase_shipment.item_id = nil
+        @purchase_shipment.valid?
+        expect(@purchase_shipment.errors.full_messages).to include("Item can't be blank")
+      end
     end
   end
 end
